ba2b79e78d8885a5df9ac15b75794b4f0493bc9f
Tojaeung <kimwoon11@naver.com>
2022-08-22 20:44:39 +0900

    스프링 데이터 jpa의 페이징과 정렬 학습

--- src/main/java/com/tojaeung/datajpa/repository/MemberRepository.java
+++ src/main/java/com/tojaeung/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.tojaeung.datajpa.repository;
 
 import com.tojaeung.datajpa.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,4 +17,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      * */
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    /*
+     * 쉽게하는 스프링 데이터 jpa의 페이징과 정렬
+     * 조인이 되는 상태에서는 카운트 쿼리를 별도로 최적화해서 보내준다.
+     * 왜냐하면, 데이터 갯수만 세면 되는데 굳이... 조인을 해서 카운트를 하기 때문에 비효율적이다.
+     * */
+    @Query(value = "select m from Member m left join m.team t", countQuery = "select count(m) from Member m")
+    Page<Member> findByAge(int age, Pageable pageable);
 }
--- src/test/java/com/tojaeung/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/tojaeung/datajpa/repository/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import com.tojaeung.datajpa.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,7 +21,7 @@ class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
-    public void findUser메서드테스트() {
+    public void 레포지토리메서드_쿼리_테스트() {
         Member m1 = new Member("AAA", 10);
         Member m2 = new Member("BBB", 20);
 
@@ -28,5 +31,31 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findUser("AAA", 10);
 
         assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void 페이징과정렬_테스트() {
+        // given
+        memberRepository.save(new Member(("member1"), 10));
+        memberRepository.save(new Member(("member2"), 10));
+        memberRepository.save(new Member(("member3"), 10));
+        memberRepository.save(new Member(("member4"), 10));
+        memberRepository.save(new Member(("member5"), 10));
+
+        int age = 10;
+
+        // 페이지는 1이 아니라 0부터 시작이다.
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        // when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);   // Page, Slice(더보기, limit += 1), List(아마 무한스크롤?)
+
+        // then
+        assertThat(page.getContent().size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
     }
 }
